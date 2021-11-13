4a6009d9269e070c6cb442b24daaafd6cda66537
proyev <prots.yevhen@gmail.com>
2021-11-13 01:34:22 +0100

    Addjust sizing

--- client/assets/components/Filters/Filters.js
+++ client/assets/components/Filters/Filters.js
@@ -2,19 +2,29 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import SearchTypesFilter from '../SearchTypesFilter/SearchTypesFilter';
+import SourcesFilter from '../SourcesFilter/SourcesFilter';
 
 export default function Filters(props) {
   return (
     <View style={styles.container}>
-      <SearchTypesFilter />
+      <View style={styles.sourcesFilter}>
+        <SourcesFilter />
+      </View>
+      <View style={styles.searchTypesFilter}>
+        <SearchTypesFilter />
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'black',
-    height: 500,
-    width: '100%'
+    flex: .13
+  },
+  sourcesFilter: {
+    flex: .6
+  },
+  searchTypesFilter: {
+    flex: .4
   }
 })
