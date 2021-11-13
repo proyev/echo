b7e10589af040d2b048fd4741651ea4ffc3e3eb9
proyev <prots.yevhen@gmail.com>
2021-11-13 01:37:50 +0100

    Create component and adjust sizing

---
+++ client/assets/components/SourcesFilter/SourcesFilter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import Source from '../Source/Source';
+
+export default function SourcesFilter(props) {
+
+  return (
+    <ScrollView
+      contentContainerStyle={styles.container}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+    >
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+      <Source />
+
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'grey'
+  }
+});
