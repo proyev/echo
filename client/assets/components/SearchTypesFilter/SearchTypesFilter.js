6b12082b65cd42654d3123fcac925adf367f5132
proyev <prots.yevhen@gmail.com>
2021-11-13 01:35:44 +0100

    Create proper layout

--- client/assets/components/SearchTypesFilter/SearchTypesFilter.js
+++ client/assets/components/SearchTypesFilter/SearchTypesFilter.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 
 import SearchType from '../SearchType/SearchType';
 
 export default function SearchTypesFilter(props) {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        fadingEdgeLength={1}
-        horizontal={true}
-      >
-        <SearchType type='Top' />
-        <SearchType type='Artists' />
-        <SearchType type='Songs' />
-        <SearchType type='Albums' />
-        <SearchType type='Playlists' />
-      </ScrollView>
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+    >
+      <SearchType type='Top' />
+      <SearchType type='Artists' />
+      <SearchType type='Songs' />
+      <SearchType type='Albums' />
+      <SearchType type='Albums' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+      <SearchType type='Playlists' />
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    flexDirection: 'row',
-    height: '80%'
+    backgroundColor: 'grey'
   }
 })
