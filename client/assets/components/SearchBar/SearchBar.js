406f42a5f3a2ab5420ea17774659e38f0842eec1
proyev <prots.yevhen@gmail.com>
2021-11-13 01:39:00 +0100

    Add search bar component

---
+++ client/assets/components/SearchBar/SearchBar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function SearchBar(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <Image
+          style={styles.icon}
+          source={require('../../icon/search2.png')}
+        />
+      </View>
+      <TextInput
+        placeholder='Search...'
+        placeholderTextColor='ghostwhite'
+        onChangeText={text => props.setInput(text)}
+        style={styles.input}
+        value={props.input}
+        underlineColorAndroid='transparent'
+      />
+      {
+        props.input ?
+          <TouchableOpacity
+            onPress={() => props.setInput('')}
+            style={styles.iconContainer}
+          >
+            <Image
+              style={styles.icon}
+              source={require('../../icon/close2.png')}
+            />
+          </TouchableOpacity>
+          : <View/>
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#E0AAFF',
+    flex: .07,
+    flexDirection: 'row',
+    opacity: .5,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    flex: .2,
+    justifyContent: 'center'
+  },
+  icon: {
+    height: 24,
+    width: 24,
+    zIndex: 1
+  },
+  input: {
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    color: 'ghostwhite',
+    flex: 1
+  }
+});
