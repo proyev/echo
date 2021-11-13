b0c8ebf590d5dcd28c7f0858ddbba9bf5844fc8c
proyev <prots.yevhen@gmail.com>
2021-11-13 01:41:09 +0100

    Try avoiding keyboard shifting

--- client/App.js
+++ client/App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, ScrollView, View } from 'react-native';
+
+import SearchBar from './assets/components/SearchBar/SearchBar';
+import Filters from './assets/components/Filters/Filters';
+import PlayerBar from './assets/components/PlayerBar/PlayerBar';
 
 export default function App() {
+  const [input, setInput] = React.useState('');
+  //when keyboard pops up everything shrinks => solve
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      behavior='height'
+      style={styles.container}
+    >
+      <View style={{
+        flex: 1
+      }}>
+        <PlayerBar />
+        <SearchBar
+          input={input}
+          setInput={setInput}
+        />
+        {
+          input ?
+            <Filters />
+            : <KeyboardAvoidingView />
+        }
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#10002B'
   },
 });
