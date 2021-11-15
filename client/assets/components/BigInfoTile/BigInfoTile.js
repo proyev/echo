fdf3c857fd05fcb8390ff22c4e03f6338f8021d9
proyev <prots.yevhen@gmail.com>
2021-11-15 03:31:24 +0100

    Design component for albums and playlists

---
+++ client/assets/components/BigInfoTile/BigInfoTile.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+
+export default function BigInfoTile(props) {
+
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.innerContainer}>
+        <View style={styles.imgContainer}>
+          <Image style={[styles.img, styles.artistImg]} />
+        </View>
+        <View style={styles.infoContainer}>
+          <View style={styles.nameContainer}>
+            <Text style={styles.name}>Name</Text>
+          </View>
+          <View style={styles.yearContainer}>
+            <Text style={styles.year}>2021</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 185,
+    width: 145
+  },
+  innerContainer: {
+    flex: 1
+  },
+  imgContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 145,
+    width: 145
+  },
+  img: {
+    height: 145,
+    width: 145
+  },
+  artistImg: {
+    borderRadius: 75
+  },
+  infoContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  nameContainer: {
+    flex: .5
+  },
+  name: {
+    color: 'ghostwhite',
+    fontWeight: 'bold'
+  },
+  yearContainer: {
+    flex: .5
+  },
+  year: {
+    color: 'ghostwhite'
+  }
+});
