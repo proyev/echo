588a67a9b0eaf8cae21eeed0e329aeb3fff8b74d
proyev <prots.yevhen@gmail.com>
2021-11-15 12:21:51 +0100

    Create artist content component

---
+++ client/assets/components/ArtistContent/ArtistContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SectionList } from 'react-native';
+
+export default function ArtistContent(props) {
+
+  return (
+    <SectionList>
+
+    </SectionList>
+  )
+
+}
+
+const styles = StyleSheet.create({
+
+});
