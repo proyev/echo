8f2a9ec65e5c5f5516bf9075c9c5d213e8af56ef
proyev <prots.yevhen@gmail.com>
2021-11-15 17:20:00 +0100

    Elements layout and first design draws done

--- client/assets/components/ArtistContent/ArtistContent.js
+++ client/assets/components/ArtistContent/ArtistContent.js
@@ -1,16 +1,193 @@
 import React from 'react';
-import { SectionList } from 'react-native';
+import { SectionList, Text, StyleSheet, FlatList, View } from 'react-native';
+
+import InfoTile from '../InfoTile/InfoTile';
+import BigInfoTile from '../BigInfoTile/BigInfoTile';
 
 export default function ArtistContent(props) {
-
   return (
-    <SectionList>
-
+    <SectionList
+      stickySectionHeadersEnabled={false}
+      sections={SECTIONS}
+      keyExtractor={(item, index) => item + index}
+      renderSectionHeader={({ index, section }) => (
+        <View>
+          <View style={styles.sectionTitleContainer}>
+            <Text style={styles.sectionTitle}>{section.section}</Text>
+          </View>
+          {
+            section.section !== 'Popular' &&
+            <FlatList
+              horizontal
+              contentContainerStyle={{alignItems: 'center'}}
+              data={section.data}
+              renderItem={({ index }) => <BigInfoTile key={index} />}
+              showsHorizontalScrollIndicator={false}
+              style={styles.horizontalSection}
+            />
+          }
+        </View>
+      )}
+      renderItem={({ index, section }) => (
+        section.section === 'Popular' && <InfoTile key={index} />
+      )}
+    >
     </SectionList>
   )
 
 }
 
 const styles = StyleSheet.create({
-
+  sectionTitleContainer: {
+    justifyContent: 'center',
+    height: 45,
+    paddingHorizontal: 10,
+    width: '100%'
+  },
+  sectionTitle: {
+    color: 'ghostwhite',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  horizontalSection: {
+    height: 220,
+  }
 });
+
+const SECTIONS = [
+  {
+    section: 'Popular',
+    data: [
+      {
+        title: 'Gimme Some Love',
+      },
+      {
+        title: 'Gimme Some Love',
+      },
+      {
+        title: 'Gimme Some Love',
+      },
+      {
+        title: 'Gimme Some Love',
+      },
+      {
+        title: 'Gimme Some Love',
+      },
+      {
+        title: 'Gimme Some Love',
+      }
+    ]
+  },
+  {
+    section: 'Discography',
+    data: [
+      {
+        title: 'Boombox Bangers',
+        type: 'Single',
+        year: 2020
+      },
+      {
+        title: 'Boombox Bangers',
+        type: 'Single',
+        year: 2020
+      },
+      {
+        title: 'Boombox Bangers',
+        type: 'Single',
+        year: 2020
+      },
+      {
+        title: 'Boombox Bangers',
+        type: 'Single',
+        year: 2020
+      },
+      {
+        title: 'Boombox Bangers',
+        type: 'Single',
+        year: 2020
+      }
+    ]
+  },
+  {
+    section: 'Featuring',
+    data: [
+      {
+        title: 'Vizionn Radio',
+        descrition: 'LISTEN'
+      },
+      {
+        title: 'Vizionn Radio',
+        descrition: 'LISTEN'
+      },
+      {
+        title: 'Vizionn Radio',
+        descrition: 'LISTEN'
+      },
+      {
+        title: 'Vizionn Radio',
+        descrition: 'LISTEN'
+      },
+      {
+        title: 'Vizionn Radio',
+        descrition: 'LISTEN'
+      }
+    ]
+  },
+  {
+    section: 'Playlists',
+    data: [
+      {
+        name: 'Techno Alarm'
+      },
+      {
+        name: 'Techno Alarm'
+      },
+      {
+        name: 'Techno Alarm'
+      },
+      {
+        name: 'Techno Alarm'
+      },
+      {
+        name: 'Techno Alarm'
+      }
+    ]
+  },
+  {
+    section: 'Fans also like',
+    data: [
+      {
+        name: 'CLTX'
+      },
+      {
+        name: 'CLTX'
+      },
+      {
+        name: 'CLTX'
+      },
+      {
+        name: 'CLTX'
+      },
+      {
+        name: 'CLTX'
+      },
+      {
+        name: 'CLTX'
+      }
+    ]
+  },
+  {
+    section: 'Appears on',
+    data: [
+      {
+        name: 'Boiler Room Live'
+      },
+      {
+        name: 'Boiler Room Live'
+      },
+      {
+        name: 'Boiler Room Live'
+      }
+    ]
+  }
+]
